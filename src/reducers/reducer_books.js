b24a7b932c74244678f4f845df13bcf5ca27c791
Serina Ko <serinako08@gmail.com>
2018-09-08 14:07:23 +1000

    rename component

--- src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-  return [
-    { titie: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
-  ];
-}
