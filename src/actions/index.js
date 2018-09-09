b24892b28c7c037987981894278772f92830bf9c
Serina Ko <serinako08@gmail.com>
2018-09-09 15:09:38 +1000

    create an ActionCreator which returns an action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
-function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+export function selectBook(book) {
+  // selectBook is an ActionCreator, it needs to return an action,
+  // an object with a type property.
+  return {
+    // the type is always an upper case, usually is a string, and is always separated by kewords if it's multiple words.
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
