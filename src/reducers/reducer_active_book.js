ded8283d4de50e00c1fd5bfced8a4865e4fc5a20
Serina Ko <serinako08@gmail.com>
2018-09-09 14:44:03 +1000

    default the value of the state argument to null

--- src/reducers/reducer_active_book.js
+++ src/reducers/reducer_active_book.js
@@ -1,7 +1,10 @@
 // State argument is not application state, only the state
 // this reducer is responsible for
 
-export default function(state, action) {
+// default the value of the state argument to null.
+// when this argument comes in if it's undefined, set it to null.
+
+export default function(state = null, action) {
   switch (action.type) {
     case 'BOOK_SELECTED':
       return action.payload;
